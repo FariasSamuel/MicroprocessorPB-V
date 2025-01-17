56e6f045d89c565a188f31a776004ab9756df949
larisvitoria <vitoriasantos2570@gmail.com>
2025-01-17 00:56:58 -0300

    Comentarios do compilador

--- assembler.cpp
+++ assembler.cpp
@@ -1,3 +1,4 @@
+// Inclusão de bibliotecas e adição de funcionalidades
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -5,57 +6,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// * Função de transformação de decimal (base 10) para binário (base 2)
+// Entrada: int num (número decimal inteiro a ser transformado)
+// Saída: str (string que representa a conversão binária)
 string DecimalToBinary(int num)
 {
-    string str;
-    int b = 0;
+    string str; // Armazena o número binário como texto
+    int b = 0; // Contador para controlar até 8 bits
+    // Loop de cálculo para cada bit
     while (b < 8)
     {
+        /* Processo de reescrita do número e atribuição à string:
+        O operador & já interpreta o número decimal como sua representação binária
+        e após a checagem adiciona o respectivo número (em formato de char) à string */
         if (num & 1)
             str += '1';
         else
             str += '0';
-        num >>= 1; 
-        b++;
+        num >>= 1; // Diminui o bit menos significativo do número (mais à direita)
+        b++; // Atualiza o contador
     }
+    // Inverte a string, pois a leitura é feita de forma inversa (do menos ao mais significativo)
     reverse(str.begin(), str.end());
+    // Retorna a string já convertida
     return str;
 }
 
 int main()
 {
-    ifstream sourcecode("code.asm");
-    ifstream dictinst("dict.txt");
+    // * Abertura dos arquivos para leitura
+    ifstream sourcecode("code.asm"); // Código-fonte escrito pelo usuário
+    ifstream dictinst("dict.txt"); // Dicionário com: as funções (palavras), seu tipo (alu ou não alu),
+    // quantidade de parâmetros e a equivalência em binário
+    // Criação de um arquivo de saída em binário (será interpretado pelo processador)
     ofstream assembly("bytecode.bin", ios::binary | ios::out);
-    map<string, pair<string, pair<int, int>>> instructions;
-    vector<int> comments;
-    vector<string> code;
-    string line;
-    string instruction;
 
+    // * Definição da estrutura de variáveis que serão usadas
+    // Map: função que associa chaves a valores
+    // Chave: String da função. Ex: MOV
+    // Valor: Par composto pelo opcode (operation code) e outro par de inteiros com o tipo de função
+    // e a quantidade de parâmetros. Ex: <MOV, (1, 2)>
+    // O resultado é armazenado na variável instructions
+    map<string, pair<string, pair<int, int>>> instructions;
+    vector<int> comments; // Vetor de inteiros que representam as linhas dos comentários
+    vector<string> code; // Vetor de strings que representam o código reescrito em binário
+    string line; // String que recebe a linha atual no loop de leitura
+    string instruction; // String que recebe a instrução atual no loop de leitura
+
+    // * Criação do dicionário de funções que será interpretado pelo compilador
+    // Loop que percorre linha a linha o dicionário de instruções e armazena na variável instruction 
     while (dictinst >> instruction)
     {
-        string opcode;
-        int type, parameters;
-        dictinst >> type >> parameters >> opcode;
+        string opcode; // Variável do opcode da função lida
+        int type, parameters; // Variável do tipo e parâmetro da função lida
+        dictinst >> type >> parameters >> opcode; // Lê mais três linhas e atribui às variáveis
+        // Par de opcode + tipo + quantidade de parâmetros
         pair<int, int> p = make_pair(type, parameters);
         pair<string, pair<int, int>> f = make_pair(opcode, p);
+        // Salva a instrução lida no dicionário
         instructions[instruction] = f;
     }
-
-    comments.push_back(0);
-    int n = 1;
-    while (sourcecode >> line)
+    
+    // * Identificação de comentários no código-fonte
+    comments.push_back(0); // Inicia o vetor de comentários com 0
+    int n = 1; // Contador que acompanha a linha do código lida
+    while (sourcecode >> line) // Lê cada linha do código-fonte
     {
+        // Se a linha possuir uma barra (indicativo de comentário)
         if (line[0] != '/')
         {
-            code.push_back(line);
-            comments.push_back(comments[n-1]);
-        }else{
+            code.push_back(line); // Adiciona a linha ao vetor do código reescrito em binário
+            comments.push_back(comments[n-1]); // Adiciona ao vetor de comentários o comentário atual
+        } else{ // Caso não
             comments.push_back(comments[n-1]+1);
         }
-        n++;
+        n++; // Atualiza o contador
     }
+
     for (int i = 0; i < code.size(); i++)
     {
         line = code[i];
